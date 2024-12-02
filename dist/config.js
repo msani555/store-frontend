550aa8acaa8961b99a3c8f3eb1f68bf3305d3f0f
msani555 <mahmudribadu3@mail.com>
2024-12-02 02:00:11 +0100

    setting up gh-pages

---
+++ dist/config.js
@@ -0,0 +1,3 @@
+window.config = {
+  API_BASE_URL: 'http://localhost:5050/api',
+}
--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
@@ -15,7 +14,6 @@ import 'bootstrap/dist/js/bootstrap.bundle.js'
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
 // app.use(BootstrapVue)
 // app.use(IconsPlugin)
 
