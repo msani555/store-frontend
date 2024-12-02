75ba0207766cecfaf3d7146d36c88c087ddf15f5
msani555 <mahmudribadu3@mail.com>
2024-12-02 01:20:39 +0100

    Update the card

--- src/components/LessonCard.vue
+++ src/components/LessonCard.vue
@@ -1,27 +1,31 @@
 <template>
-  <div class="border p-4 h-100 bg-light">
-    <div class="d-flex justify-content-between">
-      <div>
-        <h3>{{ lesson.subject }}</h3>
-        <p p>Location: {{ lesson.location }}</p>
-        <p>Price: ${{ lesson.price }}</p>
-        <p>Spaces: {{ lesson.spaces }}</p>
-      </div>
+  <div>
+    <div class="border p-4 bg-light">
+      <div class="d-flex justify-content-between">
+        <div>
+          <h3>{{ lesson.subject }}</h3>
+          <p p>Location: {{ lesson.location }}</p>
+          <p>Price: ${{ lesson.price }}</p>
+          <p>
+            <b>Spaces: {{ lesson.spaces }}</b>
+          </p>
+        </div>
 
-      <img
-        :src="lesson.imageUrl"
-        alt="lesson.imageUrl"
-        class="img-fluid"
-        style="width: 90px; height: 100%"
-      />
+        <img
+          :src="lesson.imageUrl"
+          alt="lesson.imageUrl"
+          class="img-fluid"
+          style="width: 90px; height: 100%"
+        />
+      </div>
+      <button
+        class="btn btn-lg btn-primary col-12"
+        @click="addToCart"
+        :disabled="lesson.spaces <= 0"
+      >
+        {{ lesson.spaces > 0 ? 'Add to Cart' : 'Sold Out' }}
+      </button>
     </div>
-    <button
-      class="btn btn-lg btn-primary col-12"
-      @click="addToCart"
-      :disabled="lesson.spaces <= 0"
-    >
-      {{ lesson.spaces > 0 ? 'Add to Cart' : 'Sold Out' }}
-    </button>
   </div>
 </template>
 
